692bce51e19ee3fd5e5dc87edbdad503fec49368
dav_true <danil.savolyuk@gmail.com>
2020-04-13 14:06:22 +0300

    com-12

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 var progress_counter = 0;
 var exp_counter = 0;
- 
+
+console.time("time");
+
+function debounce(func, wait, immediate) {
+    var timeout;
+
+    return function executedFunction() {
+        var context = this;
+        var args = arguments;
+            
+        var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+        };
+
+        var callNow = immediate && !timeout;
+
+        clearTimeout(timeout);
+
+        timeout = setTimeout(later, wait);
+
+        if (callNow) func.apply(context, args);
+        };
+        
+};
+
 function fillBar(seconds, i, maxLength) {
     const bar = $('.bar-filler').eq(i);
     const span = $('.test').eq(i)
@@ -8,7 +33,6 @@ function fillBar(seconds, i, maxLength) {
     bar.css('transition', `width ${seconds}s ease-in-out`)
     bar.css('width', maxLength + '%')
     const interval = setInterval( () => {
-        // bar.css('width', percent + '%')
         span.text(percent)
         percent++;
         if(percent > maxLength) {
@@ -37,67 +61,47 @@ function liveCounter (seconds, i, maxLength) {
 }
 
 
-$(document).ready( function () {
-    $(window).on("scroll", function() {
-        var y = $(document).scrollTop();
-        var h = $(window).height()
-        console.log(y+h)
-        
-        if(y + h > 1000 && progress_counter == 0) {
-            progress_counter++;
-            fillBar(0.7, 0, 84);
-            fillBar(1, 1, 89);
-            fillBar(1.3, 2, 81);
-        }
+function pageCoords () {
 
-
-        if(y + h > 1500 && exp_counter == 0) {
-            exp_counter++;
-            liveCounter(1, 0, 83);
-            liveCounter(1, 1, 1041);
-            liveCounter(1, 2, 123);
-            liveCounter(1, 3, 78);
-        }
-        
-        
-    });
-
-   $('.menu-button').click(function () {
-        $('.menu').css({
-            'right' : '0',
-            'transition' : 'right 0.4s ease-in-out'
-        })
-
-        $('body').css('overflow-y', 'hidden')
-    })
-
-    $('.close-button').click(function () {
-        $('.menu').css({
-            'right' : '-100%',
-            'transition' : 'right 0.4s ease-in-out'
-        })
-        
-        $('body').css('overflow-y', 'scroll')
-    })
-
-});
-window.addEventListener('DOMContentLoaded', () => {
-    var y = $(document).scrollTop();
-    console.log(y)
-    var h = $(window).height()
-
-    if(y+h > 1000) {
+    let pageY = window.pageYOffset || document.documentElement.scrollTop;
+    let windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    
+    if(pageY + windowHeight > 1000 && progress_counter == 0) {
         progress_counter++;
             fillBar(0.7, 0, 84);
             fillBar(1, 1, 89);
             fillBar(1.3, 2, 81);
     }
 
-    if(y + h > 1500) {
+    if(pageY + windowHeight > 1420 && exp_counter == 0) {
         exp_counter++;
         liveCounter(1, 0, 83);
         liveCounter(1, 1, 1041);
         liveCounter(1, 2, 123);
         liveCounter(1, 3, 78);
+        window.removeEventListener('scroll', pageCoords)
     }
+
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    pageCoords () 
+
+    const menu_close_button = document.querySelector('[data-menu-close-button]');
+    const menu_button = document.querySelector('[data-menu-button]');
+    const menu = document.querySelector('[data-menu]')
+    
+    console.timeEnd('time')
+    window.addEventListener('scroll', pageCoords); 
+    
+    menu_button.addEventListener('click', () => {
+        menu.style.right = 0;
+    })
+
+    menu_close_button.addEventListener('click', () => {
+        menu.style.right = '-100%';
+    })
+
 })
+
